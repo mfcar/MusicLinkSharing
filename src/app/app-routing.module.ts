9a468df9f58a5b90d76468b6609ad63ab56656d1
Marcos Carvalho <marcosfcar@gmail.com>
2020-10-07 11:51:12 -0300

    Fixing initial route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: '',   redirectTo: '/first-component', pathMatch: 'full' },
+  { path: '',   redirectTo: '/home', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent },
 ];
 
