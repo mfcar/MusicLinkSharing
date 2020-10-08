7f3f38f05e5561f000a6b5d2f92cd0a60021e094
Marcos Carvalho <marcosfcar@gmail.com>
2020-10-08 08:38:39 -0300

    Added Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const enablePurge =  (process.env.ENABLE_PURGE === 'true');
+
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: {
+    enabled: enablePurge,
+    content: [
+      './src/**/*.html',
+      './src/**/*.scss'
+    ]
+  },
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
