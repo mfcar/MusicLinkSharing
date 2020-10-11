a986b9f289fd5788c1b87428f8594d9494c6ab5a
Marcos Carvalho <marcosfcar@gmail.com>
2020-10-11 14:57:58 -0300

    Scaffolding for home components

---
+++ src/app/home-universal-search/home-universal-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-universal-search',
+  templateUrl: './home-universal-search.component.html',
+  styleUrls: ['./home-universal-search.component.scss']
+})
+export class HomeUniversalSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
