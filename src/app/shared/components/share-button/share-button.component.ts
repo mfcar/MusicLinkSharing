7e8aafd0ef05d99893a241dd7edb55728a3bdb64
Marcos Carvalho <marcosfcar@gmail.com>
2020-10-31 21:54:53 -0300

    Share button component

--- src/app/shared/components/share-button/share-button.component.ts
+++ src/app/shared/components/share-button/share-button.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-share-button',
   templateUrl: './share-button.component.html',
   styleUrls: ['./share-button.component.scss']
 })
-export class ShareButtonComponent implements OnInit {
+export class ShareButtonComponent {
+  @Input() title = 'WebShare API';
+  @Input() text = '';
+  @Input() url = location.href;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  clickShare(): void {
+    navigator.share({
+      title: this.title,
+      text: this.text,
+      url: this.url
+    })
+      .then(_ => {
+        console.log('Shared');
+      })
+      .catch(error => {
+        console.log(`Not shared, error:\n${error}`);
+      });
   }
-
 }
